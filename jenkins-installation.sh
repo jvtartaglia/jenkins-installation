ddd0305e3b3cfd67406ce3dd93cf5864e1c5159e
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-10 08:33:02 -0300

    added hashbang

--- jenkins-installation.sh
+++ jenkins-installation.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update && sudo apt-get upgrade
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
