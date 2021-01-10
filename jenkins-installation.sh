69e18533039cbf284e653603d432c7d32734a74f
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-10 08:36:22 -0300

    added -y

--- jenkins-installation.sh
+++ jenkins-installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get -y upgrade
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins openjdk-15-jdk
+sudo apt-get -y install jenkins openjdk-15-jdk
 sudo systemctl start jenkins
 
