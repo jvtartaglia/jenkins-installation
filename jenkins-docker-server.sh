3e3c9da9605885bc9d96e9f03409ff8e529690d1
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-11 07:48:53 -0300

    added docker install script

---
+++ jenkins-docker-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y upgrade
+
+# Docker installation
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+# Jenkins installation
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins openjdk-11-jdk
+exec bash
+sudo systemctl start jenkins
+
