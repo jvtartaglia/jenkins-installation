89666ae22335e9a3a5b2f8042dc1682c37a80657
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-11 07:18:01 -0300

    jdk version 11

--- jenkins-installation.sh
+++ jenkins-installation.sh
@@ -4,6 +4,6 @@ sudo apt-get update && sudo apt-get -y upgrade
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get -y install jenkins openjdk-14-jdk
+sudo apt-get -y install jenkins openjdk-11-jdk
 sudo systemctl start jenkins
 
