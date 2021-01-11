4e5bb2611738f6cb8f5a35236cfa5597a189a9d3
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-11 13:24:16 -0300

    removed -get from apt

--- jenkins-docker-server.sh
+++ jenkins-docker-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get -y upgrade
+sudo apt update && sudo apt -y upgrade
 
 # Docker installation
-sudo apt-get -y install \
+sudo apt -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -14,16 +14,16 @@ sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
 # Jenkins installation
-sudo apt-get -y install openjdk-11-jdk
+sudo apt -y install openjdk-11-jdk
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install -y jenkins
+sudo apt update
+sudo apt install -y jenkins
 sudo systemctl start jenkins
 sudo usermod -aG docker jenkins
 
