36792edc54ae5e5b0afe7d3305402f4d99a79b56
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-11 16:36:14 -0300

    config worker

--- jenkins-worker.sh
+++ jenkins-worker.sh
@@ -20,11 +20,3 @@ sudo usermod -aG docker $USER
 
 # Java installation
 sudo apt -y install openjdk-11-jdk
-# wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-# sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-# sudo apt update
-# sudo apt install -y jenkins
-# sudo systemctl start jenkins
-# sudo usermod -aG docker jenkins
-
-sudo reboot
