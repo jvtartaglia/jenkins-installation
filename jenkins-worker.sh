3308c5864010d78c94e1e21c0fffceb5c2a6378e
jvtartaglia <joao.v.tartaglia@gmail.com>
2021-01-14 11:23:57 -0300

    added aws cli to worker

--- jenkins-worker.sh
+++ jenkins-worker.sh
@@ -3,20 +3,15 @@
 sudo apt update && sudo apt -y upgrade
 
 # Docker installation
-sudo apt -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-sudo apt update
-sudo apt -y install docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 sudo usermod -aG docker $USER
+exec bash
 
 # Java installation
 sudo apt -y install openjdk-11-jdk
+
+# AWS CLI
+sudo apt -y awscli
+aws --version
+aws configure
